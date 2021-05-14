253a913b95ba4a109d085e99448c8146d37129e4
vitorcm2 <vitorcm2@al.insper.edu.br>
2021-05-14 10:58:11 -0300

    Movimentação e NPC seguindo

--- Bossfinal/Assets/Scripts/PlayerController.cs
+++ Bossfinal/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public Transform movePoint;
+    public LayerMask whatStopMovement;
+
+
 
     void Start()
     {
@@ -16,15 +19,24 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // press al the way to the right
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
-        {
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-        }
 
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+
+        if( Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            // press al the way to the right
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),0.2f, whatStopMovement)){
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"),0f),0.2f, whatStopMovement)){
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
         }
     }
 }
