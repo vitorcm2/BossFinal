6fd7c0e0d92caf8ee701afa985a255e8d8e242de
manucastilla <manuela.castilla@gmail.com>
2021-05-19 14:42:00 -0300

    animator

--- Bossfinal/Assets/Scripts/NPCfollow.cs
+++ Bossfinal/Assets/Scripts/NPCfollow.cs
@@ -8,19 +8,39 @@ public class NPCfollow : MonoBehaviour
     public float speed;
     private Transform target;
     private bool to_follow;
+
+    public Animator animator;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
         to_follow = true;
         speed = 4.5f;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (to_follow){
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        if (to_follow)
+        {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            {
+                animator.SetBool("isHorizontal", true);
+                animator.SetBool("isVertical", false);
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {
+                animator.SetBool("isHorizontal", false);
+                animator.SetBool("isVertical", true);
+            }
         }
+
+
+
     }
 }
