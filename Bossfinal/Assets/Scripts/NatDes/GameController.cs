50520ff48b29eb29ba3c522d8e462a8c5c77a270
manucastilla <manuela.castilla@gmail.com>
2021-05-28 08:55:47 -0300

    comecei o memoryGame

---
+++ Bossfinal/Assets/Scripts/NatDes/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    public List<Button> btns = new List<Button>();
+    void Start()
+    {
+        GetButtons();
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+        }
+
+    }
+}
