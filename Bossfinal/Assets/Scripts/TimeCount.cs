a6f2dbdc365bd107770b3b8c98eab9a64b083fc4
vitorcm2 <vitorcm2@al.insper.edu.br>
2021-06-04 00:03:10 -0300

    Cronometro do jogo

--- Bossfinal/Assets/Scripts/TimeCount.cs
+++ Bossfinal/Assets/Scripts/TimeCount.cs
@@ -7,17 +7,30 @@ public class TimeCount : MonoBehaviour
 
     public Text textTimer;
     GameManager gm;
+    private float startTime;
     void Start()
     {
         gm = GameManager.GetInstance();
-        DontDestroyOnLoad(transform.gameObject);
+        // DontDestroyOnLoad(transform.gameObject);
+        if (gm.gameState == GameManager.GameState.GAME){
+            startTime = Time.time;
+        }
+
     }
     void Update()
     {
         // Debug.Log(textTimer.text);
-        int minutes = Mathf.FloorToInt(gm.time / 60F);
-        int seconds = Mathf.FloorToInt(gm.time - (minutes * 60));
-        string clockTime = string.Format("{0:00}:{1:00}", minutes, seconds);
-        textTimer.text = $"Time: {clockTime}";
+        // int minutes = Mathf.FloorToInt(gm.time / 60F);
+        // int seconds = Mathf.FloorToInt(gm.time - (minutes * 60));
+        // string clockTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+        // textTimer.text = $"Time: {clockTime}";
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        textTimer.text = $"Time: {minutes}:{seconds}";
+
     }
 }
